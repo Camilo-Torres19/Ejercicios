22b3483b28ffb3aefa38ec7b051cc4b4226bf056
Camilo A. Torres O. <camilo.torreso@unilibre.edu.co>
2024-03-13 21:40:04 -0500

    Modificaciones4

--- Ejercicio59.java
+++ Ejercicio59.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Ejercicio59 {
     public static void main(String []abc){
         Scanner sc = new Scanner(System.in);
-        int numIngresado; 
+        int num_Ingresado; 
 
         do {
             System.out.println("Ingresa un numero del 1 al 10");
-            numIngresado = sc.nextInt(); 
-        } while (numIngresado <= 0 || numIngresado > 10);
+            num_Ingresado = sc.nextInt(); 
+        } while (num_Ingresado <= 0 || num_Ingresado > 10);
         sc.close();
 
         for (int i = 0; i <= 10; i++) {
-            System.out.println(numIngresado+ " X "+ i + " = "+ (numIngresado*i));
+            System.out.println(num_Ingresado+ " X "+ i + " = "+ (num_Ingresado*i));
         }
     }
 }
