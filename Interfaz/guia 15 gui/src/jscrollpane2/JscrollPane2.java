02872dc6e71e95d55d8df3a2c26f816e1c1ce8db
Camilo A. Torres O. <camilo.torreso@unilibre.edu.co>
2024-04-25 17:12:56 -0500

    2doCorte

---
+++ Interfaz/guia 15 gui/src/jscrollpane2/JscrollPane2.java
@@ -0,0 +1,20 @@
+
+package jscrollpane2;
+
+/**
+ *
+ * @author Camilo Torres
+ */
+public class JscrollPane2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal principal = new Principal();
+        principal.setLocationRelativeTo(null);
+        principal.setVisible(true);
+    }
+    
+}
