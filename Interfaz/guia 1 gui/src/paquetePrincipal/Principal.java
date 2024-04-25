2f66c83608df1d6b9fa15769d1e990fd97cc6354
Camilo A. Torres O. <camilo.torreso@unilibre.edu.co>
2024-04-25 17:02:01 -0500

    2doCorte

---
+++ Interfaz/guia 1 gui/src/paquetePrincipal/Principal.java
@@ -0,0 +1,20 @@
+
+package paquetePrincipal;
+
+import paqueteVista.MainWindow;
+
+/**
+ *
+ * @author Camilo Torres
+ */
+public class Principal {
+public static void main(String[] args){
+        
+    
+MainWindow ventana = new MainWindow();
+ventana.setLocationRelativeTo(null);
+ventana.setVisible(true);
+        
+    }
+    
+}
---
+++ Interfaz/guia 7 gui/src/jogglebuttons/tbtnInstalacion.java
@@ -0,0 +1,14 @@
+
+package jogglebuttons;
+
+/**
+ *
+ * @author Camilo Torres
+ */
+class tbtnInstalacion {
+
+    static void setSelected(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
